fbe5921ac4c0450777b43e165e2a336e66160538
vanpine <123456@qq.com>
2026-03-05 14:50:16 +0800

    解决知识库冗余问题

--- mcp-client/src/main/java/com/vanpine/mcpclient/config/InitVectorDatabaseConfig.java
+++ mcp-client/src/main/java/com/vanpine/mcpclient/config/InitVectorDatabaseConfig.java
@@ -11,12 +11,15 @@ import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.util.Set;
 
 @Configuration
 @RequiredArgsConstructor
@@ -26,6 +29,7 @@ public class InitVectorDatabaseConfig {
 
     // 向量数据库的操作类
     private final VectorStore vectorStore;
+    private final StringRedisTemplate stringRedisTemplate;
 
     @Value("classpath:ops.txt")
     private Resource opsFile;
@@ -35,13 +39,31 @@ public class InitVectorDatabaseConfig {
         // 1. 直接用 Resource 构造 TextReader
         TextReader textReader = new TextReader(opsFile);
         textReader.setCharset(StandardCharsets.UTF_8);
-
         // 2. 文件转换为向量（开启分词）
         List<Document> list = new TokenTextSplitter().transform(textReader.read());
         log.info("成功读取 ops.txt，分割为 {} 个文档片段", list.size());
-
-        // 3. 写入向量数据库 redisStack
+        log.info("这是文件内容{}", list.stream().map(Document::getId).toList());
+        // 删除旧向量
+        deleteKeysWithCustomPrefix();
+        // 4. 写入向量数据库 redisStack
         vectorStore.add(list);
         log.info("成功将 {} 个文档片段写入 Redis Stack 向量库", list.size());
+    }
+
+    /*
+     *  每一次更新知识库的时候 进行删除旧的向量
+     * */
+    public long deleteKeysWithCustomPrefix() {
+        // 1. 匹配所有以custom-prefix为前缀的Key（*是通配符，匹配任意后缀）
+        Set<String> keys = stringRedisTemplate.keys("custom-prefix*");
+        log.debug("{} \n", keys);
+        if (keys == null || keys.isEmpty()) {
+            System.out.println("未找到以custom-prefix为前缀的Key");
+            return 0;
+        }
+        // 2. 批量删除匹配到的Key（delete支持批量删除Set集合）
+        long deletedCount = stringRedisTemplate.delete(keys);
+        System.out.println("成功删除 " + deletedCount + " 个以custom-prefix为前缀的Key");
+        return deletedCount;
     }
 }
--- mcp-client/src/main/java/com/vanpine/mcpclient/controller/AiChatController.java
+++ mcp-client/src/main/java/com/vanpine/mcpclient/controller/AiChatController.java
@@ -1,31 +1,30 @@
 package com.vanpine.mcpclient.controller;
 
-import kotlin.text.CharCategory;
+
+import com.vanpine.mcpclient.Pojo.ChatPO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.api.Advisor;
 import org.springframework.ai.rag.advisor.RetrievalAugmentationAdvisor;
 import org.springframework.ai.rag.retrieval.search.VectorStoreDocumentRetriever;
 import org.springframework.ai.vectorstore.VectorStore;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 
 @RestController
-@RequestMapping("/ai")
 @RequiredArgsConstructor
-public class RagController {
+@RequestMapping("/api")
+public class AiChatController {
 
-    /*
-     *  demo
-     * */
-    private final ChatClient chatClient;
+    private final ChatClient dashScopeChatClient;
     private final VectorStore vectorStore;
 
-    @GetMapping("/rag")
-    public Flux<String> rag(String msg) {
-        String systemInfo = "你是一个运维工程师，按照给出的编码给出对应故障解释，否则找不到信息";
+    // AI医生对话
+    @PostMapping(value = "/chat", produces = "text.html/utf-8")
+    public Flux<String> chat(@RequestBody ChatPO chatPO) {
+        /*
+         *  Rag检索增强
+         * */
         Advisor retrievalAugmentationAdvisor = RetrievalAugmentationAdvisor.builder()
                 .documentRetriever(VectorStoreDocumentRetriever.builder()
                         .similarityThreshold(0.50)
@@ -33,11 +32,14 @@ public class RagController {
                         .build())
                 .build();
 
-        return chatClient.prompt()
-                .advisors(retrievalAugmentationAdvisor)
-                .system(systemInfo)
-                .user(msg)
-                .stream()
+
+        return dashScopeChatClient.prompt()
+                .user(chatPO.getMessage())
+                .advisors(a -> a
+                        .param("CONVERSATION_ID", chatPO.getConversationId()) // 对话记忆
+                        .advisors(retrievalAugmentationAdvisor) // RAG检索增强
+                ).stream()
                 .content();
     }
+
 }
